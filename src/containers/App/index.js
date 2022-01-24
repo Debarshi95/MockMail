d4fa9d42292114eb2d189cd9366b3132488e540d
Debarshi Bhattacharjee <devdbhatt0@gmail.com>
2022-01-24 21:31:40 +0530

    Initial setup

---
+++ src/containers/App/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div className="bg-slate-800">Hello</div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import './styles/tailwind.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
